221e6ee351ceba60aa04521b436ee8b0930ea816
marcelo.tomazini <marcelo.tomazini@objective.com.br>
2015-04-23 10:53:06 -0300

    preparing for release

--- anytime-chess-android-app/src/main/java/com/nullpointergames/anytimechess/layouts/NPGToolbar.java
+++ anytime-chess-android-app/src/main/java/com/nullpointergames/anytimechess/layouts/NPGToolbar.java
@@ -10,7 +10,6 @@ public class NPGToolbar extends Toolbar {
 	public NPGToolbar(Context context) {
 		super(context);
         setBackgroundResource(getResources().getIdentifier("ColorPrimary", "color", context.getPackageName()));
-        setElevation(4);
         setPadding(0, 45, 0, 0);
 
         Toolbar.LayoutParams layoutParams = new Toolbar.LayoutParams(Toolbar.LayoutParams.MATCH_PARENT, Toolbar.LayoutParams.WRAP_CONTENT);
